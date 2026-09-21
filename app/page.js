d7b33a7464d98cf79d288735f145c4cfbc36663b
Marina Pinheiro <161101121+Marimary0208@users.noreply.github.com>
2026-09-21 23:27:17 +0000

    feat: running the development server - #9

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@
   function Header({ title }) {
     return <h1>{title ? title : 'Default title'}</h1>;
   }
-
+  export default function HomePage() {
+ 
   function HomePage() {
       const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
       
@@ -24,4 +25,4 @@
         </div>
       );
     }
- 
+  }
