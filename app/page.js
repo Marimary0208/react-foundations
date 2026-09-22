e7dc904e87a4935e5dca005057a0b14ab232a46f
Marina Pinheiro <161101121+Marimary0208@users.noreply.github.com>
2026-09-22 00:46:27 +0000

    feat: importando o likebutton - #10

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import LikeButton from './like-button';
 
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
@@ -21,8 +21,7 @@ export default function HomePage() {
             <li key={name}>{name}</li>
           ))}
         </ul>
-
-        <button onClick={handleClick}>Like ({likes})</button>
+        <LikeButton />
       </div>
     );
   }
